26556f3ce4e4a210778657ea0166ce8ae6c316db
Manish Kumar <manish.hit2800@gmail.com>
2021-01-15 23:07:20 +0530

    done without styling only

--- app.js
+++ app.js
@@ -2,7 +2,28 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output")
 
+  var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+  function getTranslationURL(text) {
+      return serverURL + "?" + "text=" + text
+  }
+
+  function errorHandler(error) {
+      console.log("error occured", error);
+      alert("Something wrong with the server try it again after some time")
+  }
+
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
-    outputDiv.innerText = "ajsajsajssjsj "+ txtInput.value;
+    var inputText = txtInput.value;  // taking input
+
+    //calling server for processing
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler)
 })
