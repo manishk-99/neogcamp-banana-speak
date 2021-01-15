e0380971b7a658843a24065248b75b61ef99433c
Manish Kumar <manish.hit2800@gmail.com>
2021-01-15 19:04:15 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
+var outputDiv = document.querySelector("#output")
+
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
-    console.log("Clicked!");
-    console.log("input", txtInput.value);
+    outputDiv.innerText = "ajsajsajssjsj "+ txtInput.value;
 })
